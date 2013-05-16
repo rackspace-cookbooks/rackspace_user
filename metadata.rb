6e55c566f200a16e74d33c0ccb471326680b4053
Thomas Cate <tcate@workstation.rackspace>
2013-05-16 10:30:22 -0500

    incrementing version for updates

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "thomas.cate@rackspace.com"
 license          "All rights reserved"
 description      "Installs/Configures key-updater"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
