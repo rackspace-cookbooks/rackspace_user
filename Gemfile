9490e34a710eb15dd6bc2e2f23a12454ce908347
Thomas Cate <tcate@workstation.rackspace>
2013-05-17 10:25:43 -0500

    moving information from data_bag to attributes

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'berkshelf'
+gem 'test-kitchen', git: 'git://github.com/opscode/test-kitchen.git', branch: '1.0'
+gem 'kitchen-openstack'
+gem 'kitchen-vagrant', :group => :integration
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,35 +7,29 @@
 # All rights reserved - Do Not Redistribute
 #
 
-data_bag_name = node['rackspace-user']['data_bag_name']
-id            = node['rackspace-user']['user']
-password_hash = node['rackspace-user']['password_hash']
-
-keys = data_bag_item(data_bag_name, id)
-
-user keys["user"] do
-  home keys["home_folder"]
-  shell keys["shell"]
-  password password_hash
+user user do
+  home node['rackspace-user']['home_folder']
+  shell node['rackspace-user']['shell']
+  password node['rackspace-user']['password_hash']
 end
 
-directory keys["home_folder"] do
-  owner keys["user"]
-  group keys["user"]
+directory node['rackspace-user']["home_folder"] do
+  owner node['rackspace-user']['user']
+  group node['rackspace-user']['user']
   mode "0700"
   action :create
 end
 
-directory keys["home_folder"] + "/.ssh" do
-  owner keys["user"]
-  group keys["user"]
+directory node['rackspace-user']['home_folder'] + "/.ssh" do
+  owner node['rackspace-user']['user']
+  group node['rackspace-user']['user']
   mode  "0700"
   action :create
 end
 
-remote_file keys["home_folder"] + "/.ssh/authorized_keys" do
-  owner keys["user"]
-  group keys["user"]
-  source keys["remote_file"]
+remote_file node['rackspace-user']["home_folder"] + "/.ssh/authorized_keys" do
+  owner node['rackspace-user']['user']
+  group node['rackspace-user']['user']
+  source node['rackspace-user']['remote_file']
   action :create
 end
