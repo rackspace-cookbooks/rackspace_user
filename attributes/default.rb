a9130ca19b92046edf0afb7e97639977d92368b0
Zachary Deptawa <zdeptawa@gmail.com>
2014-01-14 11:25:57 -0600

    reworked rack user attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["rackspace-user"]["user"] = 'rack'
-default["rackspace-user"]["remote_file"] = 'https://raw.github.com/rackops/authorized_keys/master/authorized_keys'
-default["rackspace-user"]["shell"] = '/bin/bash'
-default["rackspace-user"]["home_folder"] = "/home/#{node['rackspace-user']['user']}"
+default[:rackspace-user][:rack_user][:user_name] = 'rack'
+default[:rackspace-user][:rack_user][:remote_file] = 'https://raw.github.com/rackops/authorized_keys/master/authorized_keys'
+default[:rackspace-user][:rack_user][:shell] = '/bin/bash'
+default[:rackspace-user][:rack_user][:home_folder] = "/home/#{node[:rackspace-user][:rack_user][:user]}"
