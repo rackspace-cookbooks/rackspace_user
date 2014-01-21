61e1f9f14879f2cbb7f3c671376daa7780ab54a7
Zachary Deptawa <zdeptawa@gmail.com>
2014-01-21 16:36:21 -0600

    changed symbols back to strings

--- recipes/additional_users.rb
+++ recipes/additional_users.rb
@@ -8,40 +8,40 @@
 #
 
 # rubocop:disable BlockNesting
-if node[:rackspace_user][:users]
-  node[:rackspace_user][:users].each do |user, data|
-    unless node[:rackspace_user][:users]["#{user}"][:enabled] == false
-      if node[:rackspace_user][:users]["#{user}"][:group].nil?
+if node['rackspace_user']['users']
+  node['rackspace_user']['users'].each do |user, data|
+    unless node['rackspace_user']['users']["#{user}"]['enabled'] == false
+      if node['rackspace_user']['users']["#{user}"]['group'].nil?
         user_group = "#{user}"
       else
-        group node[:rackspace_user][:users]["#{user}"][:group] do
+        group node['rackspace_user']['users']["#{user}"]['group'] do
           action :create
         end
-        user_group = node[:rackspace_user][:users]["#{user}"][:group]
+        user_group = node['rackspace_user']['users']["#{user}"]['group']
       end
 
       user "#{user}" do
-        unless node[:rackspace_user][:users]["#{user}"][:uid].nil?
-          uid node[:rackspace_user][:users]["#{user}"][:uid]
+        unless node['rackspace_user']['users']["#{user}"]['uid'].nil?
+          uid node['rackspace_user']['users']["#{user}"]['uid']
         end
 
-        unless node[:rackspace_user][:users]["#{user}"][:password].nil?
-          password node[:rackspace_user][:users]["#{user}"][:password]
+        unless node['rackspace_user']['users']["#{user}"]['password'].nil?
+          password node['rackspace_user']['users']["#{user}"]['password']
         end
 
-        unless node[:rackspace_user][:users]["#{user}"][:group].nil?
-          gid node[:rackspace_user][:users]["#{user}"][:group]
+        unless node['rackspace_user']['users']["#{user}"]['group'].nil?
+          gid node['rackspace_user']['users']["#{user}"]['group']
         end
 
-        shell node[:rackspace_user][:users]["#{user}"][:shell]
-        home node[:rackspace_user][:users]["#{user}"][:home]
-        comment node[:rackspace_user][:users]["#{user}"][:note]
+        shell node['rackspace_user']['users']["#{user}"]['shell']
+        home node['rackspace_user']['users']["#{user}"]['home']
+        comment node['rackspace_user']['users']["#{user}"]['note']
         supports manage_home: false
         action :create
       end
 
-      unless node[:rackspace_user][:users]["#{user}"][:groups].nil?
-        node[:rackspace_user][:users]["#{user}"][:groups].each do |supp_group|
+      unless node['rackspace_user']['users']["#{user}"]['groups'].nil?
+        node['rackspace_user']['users']["#{user}"]['groups'].each do |supp_group|
           Chef::Log.info('Trying to add group: ' + supp_group)
           group supp_group do
             action :create
@@ -51,8 +51,8 @@ if node[:rackspace_user][:users]
         end
       end
 
-      if node[:rackspace_user][:users]["#{user}"][:manage_home] == true
-        directory node[:rackspace_user][:users]["#{user}"][:home] do
+      if node['rackspace_user']['users']["#{user}"]['manage_home'] == true
+        directory node['rackspace_user']['users']["#{user}"]['home'] do
           owner "#{user}"
           group user_group
           mode '0755'
@@ -60,27 +60,27 @@ if node[:rackspace_user][:users]
           action :create
         end
 
-        unless node[:rackspace_user][:users]["#{user}"][:authorized_keys].nil?
-          directory node[:rackspace_user][:users]["#{user}"][:home] + '/.ssh' do
+        unless node['rackspace_user']['users']["#{user}"]['authorized_keys'].nil?
+          directory node['rackspace_user']['users']["#{user}"]['home'] + '/.ssh' do
             owner "#{user}"
             group user_group
             mode '0700'
             action :create
           end
 
-          template node[:rackspace_user][:users]["#{user}"][:home] + '/.ssh/authorized_keys' do
+          template node['rackspace_user']['users']["#{user}"]['home'] + '/.ssh/authorized_keys' do
             source 'authorized_keys.erb'
             mode '0600'
             owner "#{user}"
             group user_group
             variables(
-              keys: node[:rackspace_user][:users]["#{user}"][:authorized_keys]
+              keys: node['rackspace_user']['users']["#{user}"]['authorized_keys']
             )
           end
         end
       end
 
-      if node[:rackspace_user][:users]["#{user}"][:sudo] == true
+      if node['rackspace_user']['users']["#{user}"]['sudo'] == true
         sudo "#{user}" do
           user "#{user}"
           nopasswd true
@@ -91,9 +91,9 @@ if node[:rackspace_user][:users]
 end
 # rubocop:enable BlockNesting
 
-node.default[:authorization][:sudo][:include_sudoers_d] = true
+node.default['authorization']['sudo']['include_sudoers_d'] = true
 
-prefix = node[:authorization][:sudo][:prefix]
+prefix = node['authorization']['sudo']['prefix']
 begin
   t = resources(template: "#{prefix}/sudoers")
   t.source 'sudoers.erb'
--- recipes/rack_user.rb
+++ recipes/rack_user.rb
@@ -7,31 +7,31 @@
 # All rights reserved - Do Not Redistribute
 #
 
-user node[:rackspace_user][:rack_user][:user] do
-  home node[:rackspace_user][:rack_user][:home_folder]
-  shell node[:rackspace_user][:rack_user][:shell]
-  if node[:rackspace_user][:rack_user].key? 'password_hash'
-    password node[:rackspace_user][:rack_user][:password_hash]
+user node['rackspace_user']['rack_user']['user'] do
+  home node['rackspace_user']['rack_user']['home_folder']
+  shell node['rackspace_user']['rack_user']['shell']
+  if node['rackspace_user']['rack_user'].key? 'password_hash'
+    password node['rackspace_user']['rack_user']['password_hash']
   end
 end
 
-directory node[:rackspace_user][:rack_user][:home_folder] do
-  owner node[:rackspace_user][:rack_user][:user]
-  group node[:rackspace_user][:rack_user][:user]
+directory node['rackspace_user']['rack_user']['home_folder'] do
+  owner node['rackspace_user']['rack_user']['user']
+  group node['rackspace_user']['rack_user']['user']
   mode '0700'
   action :create
 end
 
-directory "#{node[:rackspace_user][:rack_user][:home_folder]}/.ssh" do
-  owner node[:rackspace_user][:rack_user][:user]
-  group node[:rackspace_user][:rack_user][:user]
+directory "#{node['rackspace_user']['rack_user']['home_folder']}/.ssh" do
+  owner node['rackspace_user']['rack_user']['user']
+  group node['rackspace_user']['rack_user']['user']
   mode  '0700'
   action :create
 end
 
-remote_file "#{node[:rackspace_user][:rack_user][:home_folder]}/.ssh/authorized_keys" do
-  owner node[:rackspace_user][:rack_user][:user]
-  group node[:rackspace_user][:rack_user][:user]
+remote_file "#{node['rackspace_user']['rack_user']['home_folder']}/.ssh/authorized_keys" do
+  owner node['rackspace_user']['rack_user']['user']
+  group node['rackspace_user']['rack_user']['user']
   source 'https://raw.github.com/rackops/authorized_keys/master/authorized_keys'
   action :create
 end
