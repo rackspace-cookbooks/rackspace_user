b34fb26f7ad1f64de14b92b00563006edb54b188
Thomas Cate <thomas.cate@rackspace.com>
2014-03-13 16:33:47 -0500

    revert version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Adds/configures users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.1'
 
 recipe 'rackspace_user', 'Configures users'
 
